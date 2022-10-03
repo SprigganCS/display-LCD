e0cbbfcfe070adc4c46e532730d26ce83e90052c
André Neves Medeiros <69917573+SprigganCS@users.noreply.github.com>
2022-10-03 18:01:01 -0300

    resizable print for all digits

--- main.py
+++ main.py
@@ -2,42 +2,111 @@ while(1):
   try:
     aux = input()
     aux = aux.split()
-    size = aux[0]
+    size = int(aux[0])
     nums = aux[1][:]
+    if size == 0:
+      break
   except EOFError as e:
     break
 
 
-  print(len(nums)) #quantidade de letras
-  print(nums,"\n") #letras
+  #print(len(nums)) #quantidade de letras
+  #print(nums,"\n") #letras
 
 
   #inicio da rotina
   for i in nums:
     
     if i == "0":
-      print(" ",   "-","\n",
-                 "|","|","\n ",
-                   "-")
+      print(" ", "-"*size)
+      for i in range(size):
+        print("","|"," "*(size-2),"|")
+
+      for i in range(size):
+        print("","|"," "*(size-2),"|")
+      print(" ", "-"*size)
+
 
     elif i == "1":
-      print("|", "\n","|")
+      print(" "*(size+2))
+      for i in range(size):
+        print(" "*(size+1),"|")
+      print(" "*(size+2))
+      for i in range(size):
+        print(" "*(size+1),"|")
+
+
     elif i == "2":
-      print("dois")
+      print(" ", "-"*size)
+      for i in range(size):
+        print(" "*(size+1),"|")
+      print(" ", "-"*size)
+      for i in range(size):
+        print("","|"," "*(size+1))
+      print(" ", "-"*size)
+
     elif i == "3":
-      print("tres")
+      print(" ", "-"*size)
+      for i in range(size):
+        print(" "*(size+1),"|")
+      print(" ", "-"*size)
+      for i in range(size):
+        print(" "*(size+1),"|")
+      print(" ", "-"*size)
+    
     elif i == "4":
-      print("quatro")
+      print(" "*(size+2))
+      for i in range(size):
+        print("","|"," "*(size-2),"|")
+      print(" ", "-"*size)
+      for i in range(size):
+        print(" "*(size+1),"|")
+
     elif i == "5":
-      print("cinco")
+      print(" ", "-"*size)
+      for i in range(size):
+        print("","|"," "*(size+1))
+      print(" ", "-"*size)
+      for i in range(size):
+        print(" "*(size+1),"|")
+      print(" ", "-"*size)
+
     elif i == "6":
-      print("seis")
+      print(" ", "-"*size)
+      for i in range(size):
+        print("","|"," "*(size+1))
+      print(" ", "-"*size)
+      for i in range(size):
+        print("","|"," "*(size-2),"|")
+      print(" ", "-"*size)
+    
     elif i == "7":
-      print("sete")
+      print(" ", "-"*size)
+      for i in range(size):
+        print(" "*(size+1),"|")
+      print(" "*(size+2))
+      for i in range(size):
+        print(" "*(size+1),"|")
+    
     elif i == "8":
-      print("oito")
+      print(" ", "-"*size)
+      for i in range(size):
+        print("","|"," "*(size-2),"|")
+      print(" ", "-"*size)
+      for i in range(size):
+        print("","|"," "*(size-2),"|")
+      print(" ", "-"*size)
+    
     elif i == "9":
-      print("nove")
+      print(" ", "-"*size)
+      for i in range(size):
+        print("","|"," "*(size-2),"|")
+      print(" ", "-"*size)
+      for i in range(size):
+        print(" "*(size+1),"|")
+      print(" ", "-"*size)
+
+
     else:
       print("erro")
 
